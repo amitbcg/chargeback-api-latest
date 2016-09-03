55d90d57fbafe1d4e71cb189092f098beb6d323a
Amit Bansal <amitbansal26@gmail.com>
2016-09-04 00:58:30 +0530

    Refactored

--- src/main/java/com/example/MetricsFetchDemoApplication.java
+++ src/main/java/com/example/MetricsFetchDemoApplication.java
@@ -3,10 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class MetricsFetchDemoApplication {
 
 	public static void main(String[] args) {
